8ecce97c7fd5d623f2360c6dc8475a1a3c65dbf8
Brendan Brown <brendanbrown27@gmail.com>
2015-09-26 14:21:12 -0400

    added google auth with torii. added navbar

--- app/controllers/music.js
+++ app/controllers/music.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	logger: function () {
+		console.log(this, this.get('session'));
+	}.on('init')
 });
--- app/routes/music.js
+++ app/routes/music.js
@@ -4,24 +4,4 @@ export default Ember.Route.extend({
 	model () {
 		return this.store.findAll('music');
 	}
-
-	// model () {
-	// 	return this.store.findAll('music').then(function(music){
-	// 		return music;
-	// 	});
-	// },
-	// afterModel (model, transition) {
-	// 	return Ember.RSVP.all(model.getEach('comments').then(function(){
-	// 		return model;
-	// 	}));
-	// }
-	// model () {
-	// 	return Ember.RSVP.hash({
-	// 		songs: this.store.findAll('music'),
-	// 		comments: this.store.findAll('comment')
-	//     }).then(function(results){
-	//     	console.log(results);
-	//     	return results.songs;
-	//     });
-	// }
 });
