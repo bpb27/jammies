23c9f3c51c1398bee089233364bb2780bb7cfd21
Brendan Brown <brendanbrown27@gmail.com>
2015-11-11 18:19:51 -0500

    firebase.json change

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   	beforeModel: function() {
-		return this.get("session").fetch().catch();
+		return this.get("session").fetch().catch(function(){});
   	},
 
   	actions: {
